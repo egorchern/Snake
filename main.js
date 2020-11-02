726920780c873512dfc50c126655109d6c8a7cfb
Egor Chernyshev <egorch.formal@gmail.com>
2020-11-02 22:21:21 +0000

    added fruit mechanic and tweaked control flow

--- main.js
+++ main.js
@@ -7,22 +7,23 @@ let game_object, field;
 let snake_block_color = "hsl(223, 78%, 59%)";
 let block_offset = 0.4;
 let opposites = {
-    "right" : "left",
-    "left" : "right",
-    "up" : "down",
-    "down" : "up"
+    "right": "left",
+    "left": "right",
+    "up": "down",
+    "down": "up"
 };
 let frame_counter = 0;
 let eye_radius, vertical_component, horizontal_component;
-function shift_array_right_by(arr, amount){
+
+function shift_array_right_by(arr, amount) {
     let new_arr = [];
-    for (let i = 0; i < arr.length; i += 1){
+    for (let i = 0; i < arr.length; i += 1) {
         new_arr.push(0);
     }
-    for (let i = 0; i < arr.length; i += 1){
+    for (let i = 0; i < arr.length; i += 1) {
         let old_data = arr[i];
         let new_index = i + amount;
-        if(new_index < arr.length){
+        if (new_index < arr.length) {
             new_arr[new_index] = old_data;
         }
     }
@@ -31,11 +32,70 @@ function shift_array_right_by(arr, amount){
 
 }
 
+//gives random integer between min(inclusive) and max(inclusive)
+function get_random_int(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class game_field {
     constructor(size_x, size_y) {
+        this.size_x = size_x;
+        this.size_y = size_y;
         this.block_width = Math.floor(canvas.width / size_x);
         this.block_height = Math.floor(canvas.height / size_y);
+        this.occupied_squares = [];
+        this.fruit_position = [];
         
+        this.update_occupied_squares = function () {
+            let occupied_squares = [];
+            for (let i = 0; i < this.snake.body.length; i += 1) {
+                let current_snake_block = this.snake.body[i];
+                let position_x = Math.round((current_snake_block.left / this.block_width));
+                let position_y = Math.round((current_snake_block.top / this.block_height));
+                occupied_squares.push([position_x, position_y]);
+            }
+            this.occupied_squares = occupied_squares;
+        }
+
+        this.generate_fruit_position = function () {
+            let pair_found;
+            let x, y;
+            do {
+                pair_found = true;
+                x = get_random_int(0, this.size_x - 1);
+                y = get_random_int(0, this.size_y - 1);
+                for(let i = 0; i < this.occupied_squares.length; i += 1){
+                    let occupied_x = this.occupied_squares[i][0];
+                    let occupied_y = this.occupied_squares[i][1];
+                    if (x === occupied_x && y === occupied_y){
+                        pair_found = false;
+                        break;
+                    }
+                }
+            } while (pair_found === false)
+            this.fruit_position = [x, y];
+            
+        }
+        this.draw_fruit = function(){
+            let radius = Math.round(this.block_width * 0.28);
+            let stick_length = Math.round(this.block_height * 0.2);
+            let x_center = this.fruit_position[0] * this.block_width + horizontal_component;
+            let y_center = this.fruit_position[1] * this.block_height + vertical_component + 0.5 * stick_length;
+            ctx.save();
+            ctx.fillStyle = "hsl(120, 60%, 35%)";
+            ctx.beginPath();
+            ctx.arc(x_center, y_center, radius, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.restore();
+            ctx.save();
+            ctx.beginPath();
+            ctx.lineWidth = 3;
+            ctx.moveTo(x_center, y_center - radius);
+            ctx.lineTo(x_center + 3, y_center - radius - stick_length);
+            ctx.stroke();
+        }
         this.draw = function () {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             // Draw background
@@ -62,8 +122,9 @@ class game_field {
                 
             }
             */
-           let color = "hsl(0, 0%, 5%)";
-           for (let i = 0; i < size_y; i += 1) {
+           
+            let color = "hsl(0, 0%, 5%)";
+            for (let i = 0; i < size_y; i += 1) {
                 for (let j = 0; j < size_x; j += 1) {
                     ctx.save();
                     ctx.lineWidth = 0.5;
@@ -72,16 +133,18 @@ class game_field {
                     ctx.moveTo(j * field.block_width, i * field.block_height);
                     ctx.lineTo(j * field.block_width + field.block_width, i * field.block_height);
                     ctx.lineTo(j * field.block_width + field.block_width, i * field.block_height + field.block_height);
-                    
+
                     ctx.stroke();
-                    
+                    ctx.restore();
                 }
-                
-                
+
+
             }
+            
             this.snake.draw();
+            this.draw_fruit();
         }
-        this.initialize = function(){
+        this.initialize = function () {
             this.snake = new snake(7, 7);
             bind_input();
         }
@@ -99,49 +162,59 @@ class snake {
         this.body = [new snake_block(start_x, start_y)];
         this.directions = ["right"];
         this.head_direction = "right";
-        
-        this.grow_snake = function(){
+        this.snake_collides_with_fruit = function(){
+            if(field.occupied_squares[0][0] === field.fruit_position[0] && field.occupied_squares[0][1] === field.fruit_position[1]){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        this.grow_snake = function () {
             let x, y;
             let last_part = this.body[this.body.length - 1];
             let last_direction = this.directions[this.directions.length - 1];
             x = Math.round(last_part.left / field.block_width);
             y = Math.round(last_part.top / field.block_height);
-            if(last_direction === "left"){
+            if (last_direction === "left") {
                 this.body.push(new snake_block(x + 1, y));
-            }
-            else if(last_direction === "right"){
+            } else if (last_direction === "right") {
                 this.body.push(new snake_block(x - 1, y));
-            }
-            else if(last_direction === "up"){
+            } else if (last_direction === "up") {
                 this.body.push(new snake_block(x, y + 1));
-            }
-            else{
+            } else {
                 this.body.push(new snake_block(x, y - 1));
             }
             this.directions.push(last_direction);
         }
-        
-        this.change_direction = function(direction){
+
+        this.change_direction = function (direction) {
             let opposite = opposites[this.head_direction];
-            if(opposite != direction){
+            if (opposite != direction) {
                 this.head_direction = direction;
             }
-            
+
         }
-        this.move_in_direction = function(){
+        this.move_in_direction = function () {
             let directions_copy = this.directions;
             let head_direction = this.head_direction;
             let new_directions = shift_array_right_by(directions_copy, 1);
             new_directions[0] = head_direction;
             this.directions = new_directions;
-            console.log(this.directions);
-            for(let i = 0; i < this.body.length; i += 1){
+
+            for (let i = 0; i < this.body.length; i += 1) {
                 this.body[i].move(this.directions[i]);
             }
+            field.update_occupied_squares();
+            let collides_with_fruit = this.snake_collides_with_fruit();
+            if(collides_with_fruit === true){
+                field.generate_fruit_position();
+                this.grow_snake();
+            }
         }
-        this.draw = function(){
+        this.draw = function () {
             this.body[0].draw(this.directions[0], true);
-            for(let i = 1; i < this.body.length; i += 1){
+            for (let i = 1; i < this.body.length; i += 1) {
                 this.body[i].draw(this.directions[i]);
             }
         }
@@ -150,14 +223,14 @@ class snake {
 
 class snake_block {
     constructor(block_x, block_y) {
-        console.log(block_x, block_y);
+
         this.top = block_y * field.block_height;
         this.bottom = this.top + field.block_height;
         this.left = block_x * field.block_width;
         this.right = this.left + field.block_width;
-        
-        this.move = function(direction){
-            switch(direction){
+
+        this.move = function (direction) {
+            switch (direction) {
                 case "up":
                     this.top -= field.block_height;
                     this.bottom -= field.block_height;
@@ -176,20 +249,20 @@ class snake_block {
                     break;
             }
         }
-       
-        this.draw = function(direction, head = false){
-            
+
+        this.draw = function (direction, head = false) {
+
             ctx.save();
             ctx.fillStyle = snake_block_color;
-            ctx.fillRect(this.left + block_offset, this.top + block_offset, field.block_width - block_offset * 2, field.block_height- block_offset * 2);
+            ctx.fillRect(this.left + block_offset, this.top + block_offset, field.block_width - block_offset * 2, field.block_height - block_offset * 2);
             ctx.restore();
 
-            if(head === true){
-                if(direction === "up" || direction === "down"){
+            if (head === true) {
+                if (direction === "up" || direction === "down") {
                     let vertical_center = this.top + vertical_component;
-                    let left_eye_x = this.left + Math.round((1/4 * field.block_width));
-                    let right_eye_x = this.right - Math.round((1/4 * field.block_width));
-                    
+                    let left_eye_x = this.left + Math.round((1 / 4 * field.block_width));
+                    let right_eye_x = this.right - Math.round((1 / 4 * field.block_width));
+
                     ctx.save();
                     ctx.fillStyle = "hsl(60, 100%, 50%)";
                     ctx.beginPath();
@@ -199,42 +272,38 @@ class snake_block {
                     ctx.arc(right_eye_x, vertical_center, eye_radius, 0, 2 * Math.PI);
                     ctx.fill();
                     ctx.restore();
-                }
-                else{
+                } else {
                     let horizontal_center = this.left + horizontal_component;
-                    let left_eye_y = this.top + Math.round((1/4 * field.block_height));
-                    let right_eye_y = this.bottom - Math.round((1/4 * field.block_height));
-                    
+                    let left_eye_y = this.top + Math.round((1 / 4 * field.block_height));
+                    let right_eye_y = this.bottom - Math.round((1 / 4 * field.block_height));
+
                     ctx.save();
                     ctx.fillStyle = "hsl(60, 100%, 50%)";
                     ctx.beginPath();
                     ctx.arc(horizontal_center, left_eye_y, eye_radius, 0, 2 * Math.PI);
                     ctx.fill();
                     ctx.beginPath();
-                    ctx.arc(horizontal_center,right_eye_y, eye_radius, 0, 2 * Math.PI);
+                    ctx.arc(horizontal_center, right_eye_y, eye_radius, 0, 2 * Math.PI);
                     ctx.fill();
                     ctx.restore();
                 }
             }
-            
+
         }
     }
 }
 
-function bind_input(){
-    document.addEventListener("keydown", function(ev){
+function bind_input() {
+    document.addEventListener("keydown", function (ev) {
         let key = ev.key;
         let direction;
-        if(key === "a"){
+        if (key === "a") {
             direction = "left";
-        }
-        else if(key === "w"){
+        } else if (key === "w") {
             direction = "up";
-        }
-        else if(key === "d"){
+        } else if (key === "d") {
             direction = "right";
-        }
-        else if(key === "s"){
+        } else if (key === "s") {
             direction = "down";
         }
         field.snake.change_direction(direction);
@@ -242,14 +311,14 @@ function bind_input(){
 }
 
 // process_frame has to be in a global scope, otherwise it does not work
-function process_frame(){
-    
+function process_frame() {
+
     field.draw();
 
 }
 
 
-function move_snake(){
+function move_snake() {
     field.snake.move_in_direction();
 }
 
@@ -258,8 +327,8 @@ function move_snake(){
 function init(difficulty) {
     canvas = document.querySelector("#main_canvas");
     ctx = canvas.getContext("2d");
-    
-    
+
+
     // easy = 200; medium = 175; hard = 120;
     switch (difficulty) {
         case "easy":
@@ -275,15 +344,15 @@ function init(difficulty) {
             move_interval = 175;
             break;
     }
-    
+
     field = new game_field(15, 15);
     field.initialize();
-    eye_radius = Math.round((1/8 * field.block_width));
+    eye_radius = Math.round((1 / 8 * field.block_width));
     vertical_component = Math.round(field.block_height / 2);
     horizontal_component = Math.round(field.block_width / 2);
     frame_timer = setInterval(process_frame, frame_interval);
     move_interval = setInterval(move_snake, move_interval);
-    
+    field.generate_fruit_position();
 }
 
 // event listener to call init() function
